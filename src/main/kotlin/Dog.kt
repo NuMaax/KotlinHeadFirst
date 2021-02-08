793e930016741675d2cf10a2b29c81054bfce25a
NuMaax <numaax@gmail.com>
2021-02-08 21:33:53 +0300

    Initial commit

---
+++ src/main/kotlin/Dog.kt
@@ -0,0 +1,17 @@
+@file:Suppress("unused", "CanBeParameter")
+@Suppress("MemberVisibilityCanBePrivate")
+
+    //Dog(...val name: String...) это и есть конструктор
+class Dog(val name: String, var weight: Int, val breed_param: String) {
+    //можно добавить в конструктор другие значения
+    val activities = arrayOf("Walks")
+    //или изменить значение перед объявлением .toUpperCase() - преобразование
+    // букв в верхний регистр
+    val breed = breed_param.toUpperCase()
+
+    //init - зарезервированное слово, означает, что при реализации объекта
+    //Dog обязательно выполнится init
+    init {
+        println("Dog $name has been initialised")
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+class Main
+
+fun main(args: Array<String>) {
+
+    println("main() worked")
+    separatorLine()
+
+    //инициализация Dog
+    val myDog = Dog("Rex", 21, "Mixed breed")
+    println(myDog)
+}
+
+//NuMaax fun
+fun separatorLine() {
+    println("-----")
+}
